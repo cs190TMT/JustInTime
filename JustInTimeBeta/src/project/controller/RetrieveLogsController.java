e78b1c08fa073aed95084930e8bc95a8e77719c1
Ivan Wesley Chua <ivanwesleychua@yahoo.com>
2015-09-29 15:44:12 +0800

    retrieveLogsTimeSpent

--- JustInTimeBeta/src/project/controller/RetrieveLogsController.java
+++ JustInTimeBeta/src/project/controller/RetrieveLogsController.java
@@ -12,56 +12,10 @@ public class RetrieveLogsController extends Controller {
     LogsService service = new LogsService();
 
     @Override
-/*
-    protected Navigation run() throws Exception {
-        // TODO Auto-generated method stub
-        Map<String, Object> input = new RequestMap(this.request);
-        LogsClientDto logList = new LogsClientDto(); 
-        JSONObject json = new JSONObject();
-        List<LogsDto> list = new ArrayList<LogsDto>();
-        TaskCalendarDto event = new TaskCalendarDto();
-        List<TaskCalendarDto> eventList = new ArrayList<TaskCalendarDto>();
-        
-        try {
-            //logList = service.getLogsList(this.request.getParameter("projectName"));
-            list =  logList.getLogsList();
-            if(logList.getLogsList().isEmpty()) {
-                System.out.println("No records in tasklist");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            logList.getErrorList().add("Server controller error: " +e.getMessage());
-        }
-        
-        for (LogsDto i : list){
-            System.out.println("log"+ i.getTaskName());
-        }
-        
-        for (LogsDto i : list){
-                event = new TaskCalendarDto();
-                 event.setTitle(i.getTaskName());
-                 event.setStart("'2015-09-09'");
-                 eventList.add(event);
-            }
-        System.out.println("eventList size: "+ eventList.size());
-       
-        
-        for (TaskCalendarDto i : eventList){
-            System.out.println(i.getTitle());
-        }
-        
-           json.put("events", eventList);
-        json.put("logList", logList.getLogsList());    
-        json.put("errorList", logList.getErrorList());
-        response.setContentType("application/json");
-        response.getWriter().write(json.toString());
-        return null;
-    }
-*/
     
     protected Navigation run() throws Exception {
         LogsClientDto logList = new LogsClientDto(); 
-        JSONObject json = new JSONObject((String) this.requestScope("data"));
+        JSONObject json = new JSONObject((String) this.request.getReader().readLine());
         
         try {
             logList = service.getLogsList(json.getString("projectName"));
@@ -81,3 +35,50 @@ public class RetrieveLogsController extends Controller {
     }
     
 }
+
+/*
+protected Navigation run() throws Exception {
+    // TODO Auto-generated method stub
+    Map<String, Object> input = new RequestMap(this.request);
+    LogsClientDto logList = new LogsClientDto(); 
+    JSONObject json = new JSONObject();
+    List<LogsDto> list = new ArrayList<LogsDto>();
+    TaskCalendarDto event = new TaskCalendarDto();
+    List<TaskCalendarDto> eventList = new ArrayList<TaskCalendarDto>();
+    
+    try {
+        //logList = service.getLogsList(this.request.getParameter("projectName"));
+        list =  logList.getLogsList();
+        if(logList.getLogsList().isEmpty()) {
+            System.out.println("No records in tasklist");
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+        logList.getErrorList().add("Server controller error: " +e.getMessage());
+    }
+    
+    for (LogsDto i : list){
+        System.out.println("log"+ i.getTaskName());
+    }
+    
+    for (LogsDto i : list){
+            event = new TaskCalendarDto();
+             event.setTitle(i.getTaskName());
+             event.setStart("'2015-09-09'");
+             eventList.add(event);
+        }
+    System.out.println("eventList size: "+ eventList.size());
+   
+    
+    for (TaskCalendarDto i : eventList){
+        System.out.println(i.getTitle());
+    }
+    
+       json.put("events", eventList);
+    json.put("logList", logList.getLogsList());    
+    json.put("errorList", logList.getErrorList());
+    response.setContentType("application/json");
+    response.getWriter().write(json.toString());
+    return null;
+}
+*/
