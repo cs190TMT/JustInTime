e6dc7abc69b450f8001714279f8004ae7a67c6e8
dksandimas <dksandimas@gmail.com>
2015-09-29 18:53:00 +0800

    Logs Fix + Calendar Fix
    Logs Fix + Calendar Fix

--- JustInTimeBeta/src/project/controller/RetrieveTasksForCalendarController.java
+++ JustInTimeBeta/src/project/controller/RetrieveTasksForCalendarController.java
@@ -14,26 +14,32 @@ import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 import org.slim3.repackaged.org.json.JSONObject;
 
+import project.dto.LogsClientDto;
+import project.dto.LogsDto;
 import project.dto.TaskCalendarDto;
 import project.dto.TasksClientDto;
-import project.dto.TasksDto;
-import project.service.TasksService;
+//import project.dto.TasksDto;
+import project.service.LogsService;
+//import project.service.TasksService;
 
 public class RetrieveTasksForCalendarController extends Controller {
 
-    TasksService service = new TasksService();
+    //TasksService service = new TasksService();
+    LogsService service = new LogsService();
     
     @Override
     protected Navigation run() throws Exception {
         TasksClientDto taskList = new TasksClientDto(); 
-        JSONObject json = new JSONObject();
-        List<TasksDto> list = new ArrayList<TasksDto>();
+        JSONObject json = new JSONObject((String) this.requestScope("data"));
+        List<LogsDto> list = new ArrayList<LogsDto>();
         TaskCalendarDto event = new TaskCalendarDto();
         List<TaskCalendarDto> eventList = new ArrayList<TaskCalendarDto>();
+        LogsClientDto logList = new LogsClientDto(); 
         
         try {
-            taskList = service.getTaskMasterList();
-            list =  taskList.getTaskList();
+           // taskList = service.getTaskMasterList();
+            logList = service.getLogsList(json.getString("projectName"));
+            list =  logList.getLogsList();
             if(taskList.getTaskList().isEmpty()) {
                 System.out.println("No records in tasklist");
             }
@@ -41,15 +47,19 @@ public class RetrieveTasksForCalendarController extends Controller {
             e.printStackTrace();
             taskList.getErrorList().add("Server controller error: " +e.getMessage());
         }
-        
-        for (TasksDto i : list){
-            System.out.println(i.getTaskName());
+        System.out.println("hello");
+        for (LogsDto i : list){
+            System.out.println("hello" + i.getTaskName());
+            System.out.println("time" + i.getTimeSpent());
+            System.out.println("time hehe" + i.getTimeStamp());
         }
         
-        for (TasksDto i : list){
+        for (LogsDto i : list){
                 event = new TaskCalendarDto();
-                 event.setTitle(i.getTaskName());
-                 event.setStart("'2015-09-09'");
+                 event.setTitle(i.getTaskName() + "  -  " + i.getTimeSpent() + " hours" );
+                // event.setStart(start);
+                 event.setStart(i.getTimeStamp());
+               //  event.setStart("'2015-09-09'");
                  eventList.add(event);
             }
         System.out.println("eventList size: "+ eventList.size());
